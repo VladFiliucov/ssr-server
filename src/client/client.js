947bdd0500c5799a7652e24321a514bdb3c26ca3
VladFiliucov <filiucov@mail.ru>
2017-10-21 14:55:18 +0100

    Integrate Browser and Server routers.

--- src/client/client.js
+++ src/client/client.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
-import Home from './components/Home';
+import { BrowserRouter } from 'react-router-dom';
 
-hydrate(<Home />, document.getElementById('root'))
+import Routes from './Routes'
+
+hydrate(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  document.getElementById('root')
+)
